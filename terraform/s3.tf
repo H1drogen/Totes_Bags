44ecd51f4298b481d1bd3dbdb0bce66072e9298e
LazySusanoo <liam.drans@gmail.com>
2024-08-23 14:37:06 +0100

    changed bucket name to totes-processed-data

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket" "data_ingestion" {
 }
 
 resource "aws_s3_bucket" "data_transform" {
-    bucket_prefix = "totes-transform-"
+    bucket_prefix = "totes-processed-data-"
     force_destroy = true
 
     tags = {
@@ -27,25 +27,6 @@ resource "aws_s3_bucket" "code_bucket" {
         Environment = "Production"
     }
 }
-
-# resource "aws_s3_bucket" "state_bucket" {
-#     bucket = "terraform-state-bucket-for-sidley"
-
-#     tags = {
-#       Environment = "Production"
-#     }
-# }
-
-# resource "aws_s3_object" "upload_state" {
-#   bucket       = "${aws_s3_bucket.state_bucket.id}"
-#   acl          = "private"
-#   key          = "terraform.tfstate"
-#   source       = "terraform.tfstate"
-#   content_type = "application/json"
-#   depends_on = [
-#     aws_s3_bucket.state_bucket,
-#   ]
-# }
 
 terraform {
     backend "s3" {
@@ -76,43 +57,6 @@ resource "aws_s3_bucket_lifecycle_configuration" "data_ingestion_lifecycle" {
     status = "Enabled"
     }
 }
-
-
-
-# resource "aws_s3_bucket_policy" "data_ingestion_policy" {
-#     bucket = aws_s3_bucket.data_ingestion.id
-
-#     policy = <<EOF
-# {
-# "Version": "2012-10-17",
-#     "Statement": [
-#     {
-#         "Sid": "AllowPublicRead",
-#         "Effect": "Allow",
-#         "Principal": "*",
-#         "Action": "s3:GetObject",
-#         "Resource": "arn:aws:s3:::${aws_s3_bucket.data_ingestion.id}/*",
-#         "Condition": {
-#         "IpAddress": {
-#             "aws:SourceIp": "192.168.0.0/16"
-#         }
-#     }
-#     }
-#   ]
-# }
-# EOF
-# }
-    # {
-    #     "Sid": "DenyUnencryptedUploads",
-    #     "Effect": "Deny",
-    #     "Principal": "*",
-    #     "Action": "s3:PutObject",
-    #     "Resource": "arn:aws:s3:::${aws_s3_bucket.data_ingestion.id}/*",
-    #     "Condition": {
-    #     "StringNotEquals": {
-    #         "s3:x-amz-server-side-encryption": "aws:kms"
-    #     }
-    # }
 
 resource "aws_s3_object" "lambda_layer" {
   bucket = aws_s3_bucket.code_bucket.bucket
